8333fe7bc9615fc779a53484b41dbf7e0855ab7b
Artem <artembulatov485@gmail.com>
2025-07-05 02:14:21 +0300

    start

---
+++ src/main/java/ru/nikolaev/Lume/LumeApplication.java
@@ -0,0 +1,13 @@
+package ru.nikolaev.Lume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nikolaev/Lume/LumeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikolaev.Lume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
